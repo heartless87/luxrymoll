18a827f98254571d944e765a731674de83fb9f60
heartless87 <contact.mail.raj@gmail.com>
2025-10-26 16:56:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template
-from backend.home import run_home_logic
 
 app = Flask(__name__)
 
 # 🏠 Home page route
 @app.route('/')
 def home():
-    data = run_home_logic()
-    return render_template('index.html', backend_data=data)
+    return render_template('index.html')
 
 # 👤 Profile page route
 @app.route('/profile')
@@ -30,3 +28,4 @@ def listprodct():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
