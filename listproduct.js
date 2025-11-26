307abbf5af801af8c825629009f31c4af1d4abff
heartless87 <contact.mail.raj@gmail.com>
2025-11-26 11:11:09 +0530

    Update listproduct.js

--- listproduct.js
+++ listproduct.js
@@ -1,7 +1,7 @@
 console.log("[listproduct.js loaded]");
 
 // ✅ Vercel API URL (PUT YOUR URL HERE)
-const API_URL = "https://luxrymoll-pt1o.vercel.app";
+const API_URL = "https://luxrymoll-pt1o.vercel.app/products";
 
 // Selectors
 const uploadTrigger = document.getElementById("uploadTrigger");
