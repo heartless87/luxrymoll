0c8aa795bb8d93ece06a084acac22a13a6431cd1
heartless87 <contact.mail.raj@gmail.com>
2026-02-03 23:02:46 +0530

    Create checkout.js

---
+++ checkout.js
@@ -0,0 +1,14 @@
+const data = JSON.parse(localStorage.getItem("checkoutProduct"));
+
+if (!data) {
+  alert("No product found");
+  location.href = "index.html";
+}
+
+const DELIVERY_FEE = 50;
+
+const itemsTotal = data.price * data.qty;
+const finalTotal = itemsTotal + DELIVERY_FEE;
+
+document.getElementById("itemsTotal").innerText = "₹" + itemsTotal;
+document.getElementById("finalTotal").innerText = "₹" + finalTotal;
