e7d84632cb8fdb7b90b7ecf9ef4bd8792a1952c4
heartless87 <contact.mail.raj@gmail.com>
2026-02-22 05:41:35 +0530

    Update likeProduct.js

--- api/likeProduct.js
+++ api/likeProduct.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 let cachedClient = null;
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://luxrymoll.shop");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
